14a7873190907894de393e3074fb3e96c901606a
Martin Fall <martinfall@gmail.com>
2020-12-10 04:53:34 -0500

    Create Solution.java

---
+++ Java Regex/Solution.java
@@ -0,0 +1,30 @@
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+//Write your code here
+/**
+ *
+ * @author martinfall
+ */
+public class MyRegex {
+
+    String pattern
+            = "\\b(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\."
+            + "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\."
+            + "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\."
+            + "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\b";
+}
